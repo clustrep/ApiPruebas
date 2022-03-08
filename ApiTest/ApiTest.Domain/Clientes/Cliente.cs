e019c375e2a44a7254f73faa9a3a11dd63b8eeb7
Christian Lustre <clustrep@gmail.com>
2022-03-08 17:03:33 -0500

    Conectandose a la base de datos

---
+++ ApiTest/ApiTest.Domain/Clientes/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ApiTest.Domain.Data
+{
+    public partial class Cliente
+    {
+        public int CodCliente { get; set; }
+        public string NombreCompleto { get; set; }
+        public string NombreCorto { get; set; }
+        public string Abreviatura { get; set; }
+        public string Ruc { get; set; }
+        public string Estado { get; set; }
+        public string GrupoFacturacion { get; set; }
+        public DateTime? InactivoDesde { get; set; }
+        public string CodigoSap { get; set; }
+    }
+}
---
+++ ApiTest/ApiTest.Infrastructure/Data/PruebasContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using ApiTest.Domain.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace ApiTest.Infrastructure.Data
+{
+    public partial class PruebasContext : DbContext
+    {
+        public PruebasContext()
+        {
+        }
+
+        public PruebasContext(DbContextOptions<PruebasContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cliente> Cliente { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>(entity =>
+            {
+                entity.HasKey(e => e.CodCliente)
+                    .HasName("PK__Cliente__DF8324D7361C355A");
+
+                entity.ToTable("Cliente", "Adm");
+
+                entity.Property(e => e.Abreviatura)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.CodigoSap)
+                    .HasColumnName("CodigoSAP")
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Estado)
+                    .IsRequired()
+                    .HasMaxLength(1)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.GrupoFacturacion)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.InactivoDesde).HasColumnType("datetime");
+
+                entity.Property(e => e.NombreCompleto)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NombreCorto)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Ruc)
+                    .IsRequired()
+                    .HasMaxLength(11)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
