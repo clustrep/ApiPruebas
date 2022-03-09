14a5a2fafd70e5c62cb7b26a0913dc23315a1c76
Christian Lustre <clustrep@gmail.com>
2022-03-08 19:42:45 -0500

    endpoint Getclientes y PostCliente

--- ApiTest/ApiTest/Controllers/ClienteController.cs
+++ ApiTest/ApiTest/Controllers/ClienteController.cs
@@ -32,17 +32,5 @@ namespace ApiTest.Controllers
         {
             await _clientes.Post(clienteDto);
         }
-
-        // PUT api/<ClienteController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ClienteController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
